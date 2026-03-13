e963d3b3a2fd76eb25f174b53457b66fa40f4386
Mateojb8 <65664794+CodeMateo15@users.noreply.github.com>
2026-03-13 02:09:36 -0400

    data and planning for bayesian model in R

---
+++ bayesianModel.R
@@ -0,0 +1,98 @@
+# Bayesian Model Plan
+# Project: Batting vs Pitching Relationship in College Baseball
+
+# 1. Idea:
+# We want to see how batting and pitching relate to team success across multiple seasons of college baseball.
+#
+# The data only has end-of-season stats for each team.
+# Teams are also grouped into conferences.
+#
+# Because the data spans multiple years (2012-2025), we want to see how the
+# batting vs pitching relationship changes over time.
+
+# 2. What the data looks like
+#
+# Years are strange since some data doesn't exist until a certain year. For example, OBP starts in 2012, while SBPG starts in 2008.
+# So the year will be decided after picking all the stats we need. We will likely have to start in 2012 to get all the stats we want. 
+# Also the 2026 is ongoing, so might scrape that right before presentation.
+#
+# Each row will represent one team in one season.
+#
+# Example columns:
+#
+# team
+# conference
+# season
+# batting_stat
+# pitching_stat
+# team_success (W or WPCT or run differential (R (Batting) - R (Pitching)))
+#
+# Batting stat could be something like:
+# OPS or RPG
+#
+# Pitching stat could be something like:
+# ERA or R (Pitching) (Runs Allowed Pitched)
+
+# 3. Basic idea of the model
+#
+# Basic relationship:
+#
+# team_success =
+#   intercept
+#   + batting_effect * batting_stat
+#   + pitching_effect * pitching_stat
+#
+# Bayesian models estimate probability distributions for these effects.
+
+# 4. Allow the relationship to change by season
+#
+# The importance of batting and pitching might change every year. So the model will estimate separate effects for each season.
+#
+# Example:
+#
+# batting_effect_2019
+# batting_effect_2020
+# batting_effect_2021
+#
+# pitching_effect_2019
+# pitching_effect_2020
+# pitching_effect_2021
+#
+# This lets us see if batting changes over time.
+
+# 5. Conferences
+#
+# Teams belong to conferences and conferences may differ in
+# overall strength.
+#
+# Could use RPI - Conference Rankings?
+
+# 6. Priors
+#
+# Example priors:
+#
+# intercept ~ Normal()
+# batting_effect ~ Normal()
+# pitching_effect ~ Normal()
+#
+# These are just starting assumptions and the data should update them.
+
+# 7. What we want from the model
+# - how strong the batting effect is each season
+# - how strong the pitching effect is each season
+#
+# This tells us whether college baseball has become more offense-driven or pitching-driven over time.
+
+# 8. Steps to implement
+#
+# a. Load and clean the data
+#
+# b. Choose batting and pitching metrics
+#
+# c. Standardize the stats if needed
+#
+# d. Encode teams, conferences, and seasons
+#
+# e. Fit Bayesian model
+#
+# f. Extract the batting and pitching effects for each season
